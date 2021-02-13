1f2559c1bc4cfcf9bc4de8e2393c71683d76bbd9
AimerNeige <aimer.neige.soft@gmail.com>
2021-02-14 01:36:23 +0800

    update

--- python/main.py
+++ python/main.py
@@ -19,9 +19,11 @@ def question(problem):
     for option in problem['options']:
         print(option['mark'], option['data'])
     print()
-    user_answer = input("请输入你的答案")
+    user_answer = input("请输入你的答案:\t")
+    print()
     print("正确答案:\t %s" % problem['answer'])
     print("你的答案:\t %s" % user_answer)
+    print()
     exit_or_not = input("回车继续，q 退出程序")
     return exit_or_not == 'q' or exit_or_not == 'Q'
 
